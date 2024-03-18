df429811cda742ebf945e253bf9407a8140f3b64
fahadkabali <kabalifahad@gmail.com>
2024-03-18 22:40:45 +0300

    update

--- src/token_frontend/src/components/Faucet.jsx
+++ src/token_frontend/src/components/Faucet.jsx
@@ -14,10 +14,10 @@ function Faucet() {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
+      <label>Get your free DFahd tokens here! Claim 10,000 DFAD coins to your account.</label>
       <p className="trade-buttons">
         <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+          Click Here!
         </button>
       </p>
     </div>
